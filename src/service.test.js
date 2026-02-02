0c574fdb9e2c5cbc73775f5b656274c813f6f499
EMoreno65 <ethanmichaelmoreno@gmail.com>
2026-02-01 17:45:22 -0700

    Had to comment out one more line for lint to pass

--- src/service.test.js
+++ src/service.test.js
@@ -8,7 +8,7 @@ let testUserAuthToken;
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
   const registerRes = await request(app).post('/api/auth').send(testUser);
-  testUserAuthToken = registerRes.body.token;
+  // testUserAuthToken = registerRes.body.token;
 });
 
 test('login', async () => {
