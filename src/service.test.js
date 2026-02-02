801f5e77fa4e64bca6c061b1000b562c04de6da0
EMoreno65 <ethanmichaelmoreno@gmail.com>
2026-02-01 21:14:59 -0700

    Edited the login test, hopefully the workflow finally works and I have a clearly defined coverage

--- src/service.test.js
+++ src/service.test.js
@@ -3,12 +3,11 @@ const request = require('supertest');
 const app = require('./service');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
-let testUserAuthToken;
+// let testUserAuthToken;
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
-  const registerRes = await request(app).post('/api/auth').send(testUser);
-  // testUserAuthToken = registerRes.body.token;
+  await request(app).post('/api/auth').send(testUser);
 });
 
 test('login', async () => {
@@ -19,5 +18,4 @@ test('login', async () => {
   const user = { ...testUser, roles: [{ role: 'diner' }] };
   delete user.password;
   expect(loginRes.body.user).toMatchObject(user);
-  // expect(loginRes.body.token).toBe(testUserAuthToken);
 });
