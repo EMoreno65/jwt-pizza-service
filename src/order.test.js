706d4c03ded45414f97a2ed0a65cdab68c8e8f22
EMoreno65 <ethanmichaelmoreno@gmail.com>
2026-02-02 15:56:24 -0700

    Changed the required line coverage to 80 and am writing an add menu test case

--- src/order.test.js
+++ src/order.test.js
@@ -2,13 +2,13 @@ const request = require('supertest');
 const app = require('./service');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
-// let testUserAuthToken;
+let testUserAuthToken;
 // let userId;
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
   await request(app).post('/api/auth').send(testUser);
-//   testUserAuthToken = registerRes.body.token;
+  testUserAuthToken = registerRes.body.token;
 //   userId = registerRes.body.user.id;
   await request(app).put('/api/auth').send(testUser);
 //   const curr_user = loginRes.body.user;
@@ -48,3 +48,10 @@ test('Get Menu', async () => {
         }
     ]);
 });
+
+test('Add Menu Item', async () => {
+    const addMenuItemRes = await request(app)
+        .put('/api/order/menu')
+        .set('Authorization', `Bearer ${testUserAuthToken}`)
+        .send({ title: 'ExtraPizza', description: 'Description with a lotta things', image: 'pizza9.png', price: 0.0001 });
+})
