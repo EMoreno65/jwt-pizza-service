7da7d007df246899b58c365db49c1a9de7481208
EMoreno65 <ethanmichaelmoreno@gmail.com>
2026-02-02 12:33:50 -0700

    Fixed the update user test. It passes now and our code should cover more

--- src/service.test.js
+++ src/service.test.js
@@ -3,11 +3,14 @@ const request = require('supertest');
 const app = require('./service');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
-// let testUserAuthToken;
+let testUserAuthToken;
+let userId;
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
-  await request(app).post('/api/auth').send(testUser);
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
+  userId = registerRes.body.user.id;
 });
 
 test('login', async () => {
@@ -25,15 +28,18 @@ test('failed login', async () => {
   expect(loginRes.status).toBe(404);
 });
 
-test('Register user', async () => {
-  const newUser = { name: 'new user', email: Math.random().toString(36).substring(2, 12) + '@test.com', password: 'a' };
-  const registerRes = await request(app).post('/api/auth').send(newUser);
-  expect(registerRes.status).toBe(200);
-});
+// test('Register user', async () => {
+//   const newUser = { name: 'new user', email: Math.random().toString(36).substring(2, 12) + '@test.com', password: 'a' };
+//   const registerRes = await request(app).post('/api/auth').send(newUser);
+//   expect(registerRes.status).toBe(200);
+// });
 
 test('Update a current user', async () => {
   const UpdatedUser = { name: 'Updated User', email: 'somethingElse@test.com', password: 'b' };
-  const UpdateRes = ((await request(app).put('/api/auth/:userId')).send(UpdatedUser));
-  expect(UpdateRes.status).toBe(200); 
-  expect(UpdateRes.body.user).toMatchObject(UpdatedUser);
+  const updateRes = await request(app)
+    .put(`/api/user/${userId}`)
+    .set('Authorization', `Bearer ${testUserAuthToken}`)
+    .send(UpdatedUser);
+  expect(updateRes.status).toBe(200); 
+  expect(updateRes.body.user).toMatchObject({ id: userId, name: UpdatedUser.name, email: UpdatedUser.email });
 })
