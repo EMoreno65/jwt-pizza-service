03565fbc10e1a0c5e1980679dfa933c84abafc07
EMoreno65 <ethanmichaelmoreno@gmail.com>
2026-02-03 15:31:05 -0700

    Wrote out the mock for the user

--- src/order.test.js
+++ src/order.test.js
@@ -14,6 +14,26 @@ jest.mock('./database/database.js', () => ({
   },
 }));
 
+let mockUser = null;
+jest.mock('./routes/authRouter.js', () => {
+  const authRouter = (req, res, next) => next();
+  authRouter.authenticateToken = (req, res, next) => {
+    if (!mockUser) return res.status(401).json({ message: 'unauthorized' });
+    req.user = mockUser;
+    req.user.isRole = (role) => !!(req.user.roles || []).find((r) => r.role === role);
+    next();
+  }
+    return {
+    setAuthUser: (req, res, next) => {
+      if (mockUser) req.user = mockUser;
+      next();
+    },
+    authRouter,
+  };
+});
+
+
+
 
 
 
