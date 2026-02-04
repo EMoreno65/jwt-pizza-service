95f2f8ff7e9fda511c03d4a8908655d21e9961a6
EMoreno65 <ethanmichaelmoreno@gmail.com>
2026-02-03 18:19:33 -0700

    I got to 77% but I'm struggling to make an effective test for updating and deleting a user. Lint should work

--- src/service.test.js
+++ src/service.test.js
@@ -14,6 +14,7 @@ function randomName() {
   return Math.random().toString(36).substring(2, 12);
 };
 
+let mockUser;
 
 jest.mock('./database/database.js', () => ({
   Role: {
@@ -87,100 +88,3 @@ test('failed login', async () => {
   const loginRes = await request(app).put('/api/auth').send({ email: mockUser.email, password: 'wrong' });
   expect(loginRes.status).toBe(500);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const request = require('supertest');
-// const app = require('./service');
-
-// const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
-// let testUserAuthToken;
-// let userId;
-
-// beforeAll(async () => {
-//   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
-//   const registerRes = await request(app).post('/api/auth').send(testUser);
-//   testUserAuthToken = registerRes.body.token;
-//   userId = registerRes.body.user.id;
-// });
-
-// test('login', async () => {
-//   const loginRes = await request(app).put('/api/auth').send(testUser);
-//   expect(loginRes.status).toBe(200);
-//   expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
-
-//   const user = { ...testUser, roles: [{ role: 'diner' }] };
-//   delete user.password;
-//   expect(loginRes.body.user).toMatchObject(user);
-// });
-
-// test('failed login', async () => {
-//   const loginRes = await request(app).put('/api/auth').send({ email: testUser.email, password: 'wrong' });
-//   expect(loginRes.status).toBe(404);
-// });
-
-// // test('Register user', async () => {
-// //   const newUser = { name: 'new user', email: Math.random().toString(36).substring(2, 12) + '@test.com', password: 'a' };
-// //   const registerRes = await request(app).post('/api/auth').send(newUser);
-// //   expect(registerRes.status).toBe(200);
-// // });
-
-// test('Update a current user', async () => {
-//   const UpdatedUser = { name: 'Updated User', email: 'somethingElse@test.com', password: 'b' };
-//   const updateRes = await request(app)
-//     .put(`/api/user/${userId}`)
-//     .set('Authorization', `Bearer ${testUserAuthToken}`)
-//     .send(UpdatedUser);
-//   expect(updateRes.status).toBe(200); 
-//   expect(updateRes.body.user).toMatchObject({ name: UpdatedUser.name, email: UpdatedUser.email });
-// });
-
-// test('Delete a user', async () => {
-//   const deleteRes = await request(app)
-//     .delete(`/api/user/${userId}`)
-//     .set('Authorization', `Bearer ${testUserAuthToken}`);
-//   expect(deleteRes.status).toBe(200); 
-//   expect(deleteRes.body).toMatchObject({ message: 'not implemented' });
-// });
-
-
-
-// // Going to try to commit again because the last one didnt work for some reason
