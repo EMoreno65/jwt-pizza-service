4809d4dea90508fbaf620012d215ea9e08af3943
EMoreno65 <ethanmichaelmoreno@gmail.com>
2026-03-17 15:15:24 -0600

    Something weird happened, trying to get it back to normal

--- src/metrics.js
+++ src/metrics.js
@@ -65,122 +65,12 @@ async function sendMetrics() {
     return;
   }
 
-  const metrics = [
-    {
-      name: 'jwt_pizza_http_request_count',
-      unit: '1',
-      sum: {
-        aggregationTemporality: 'AGGREGATION_TEMPORALITY_CUMULATIVE',
-        isMonotonic: true,
-        dataPoints: [{ asInt: httpMetrics.count, timeUnixNano: Date.now() * 1000000, attributes: [{ key: 'source', value: { stringValue: source } }] }],
-      },
-    },
-    {
-      name: 'jwt_pizza_http_error_count',
-      unit: '1',
-      sum: {
-        aggregationTemporality: 'AGGREGATION_TEMPORALITY_CUMULATIVE',
-        isMonotonic: true,
-        dataPoints: [{ asInt: httpMetrics.errors, timeUnixNano: Date.now() * 1000000, attributes: [{ key: 'source', value: { stringValue: source } }] }],
-      },
-    },
-    {
-      name: 'jwt_pizza_http_total_latency_ms',
-      unit: 'ms',
-      sum: {
-        aggregationTemporality: 'AGGREGATION_TEMPORALITY_CUMULATIVE',
-        isMonotonic: true,
-        dataPoints: [{ asInt: httpMetrics.totalLatency, timeUnixNano: Date.now() * 1000000, attributes: [{ key: 'source', value: { stringValue: source } }] }],
-      },
-    },
-    {
-      name: 'jwt_pizza_purchase_total',
-      unit: '1',
-      sum: {
-        aggregationTemporality: 'AGGREGATION_TEMPORALITY_CUMULATIVE',
-        isMonotonic: true,
-        dataPoints: [{ asInt: purchaseMetrics.total, timeUnixNano: Date.now() * 1000000, attributes: [{ key: 'source', value: { stringValue: source } }] }],
-      },
-    },
-    {
-      name: 'jwt_pizza_purchase_success',
-      unit: '1',
-      sum: {
-        aggregationTemporality: 'AGGREGATION_TEMPORALITY_CUMULATIVE',
-        isMonotonic: true,
-        dataPoints: [{ asInt: purchaseMetrics.success, timeUnixNano: Date.now() * 1000000, attributes: [{ key: 'source', value: { stringValue: source } }] }],
-      },
-    },
-    {
-      name: 'jwt_pizza_purchase_failure',
-      unit: '1',
-      sum: {
-        aggregationTemporality: 'AGGREGATION_TEMPORALITY_CUMULATIVE',
-        isMonotonic: true,
-        dataPoints: [{ asInt: purchaseMetrics.failure, timeUnixNano: Date.now() * 1000000, attributes: [{ key: 'source', value: { stringValue: source } }] }],
-      },
-    },
-    {
-      name: 'jwt_pizza_purchase_revenue',
-      unit: '1',
-      sum: {
-        aggregationTemporality: 'AGGREGATION_TEMPORALITY_CUMULATIVE',
-        isMonotonic: true,
-        dataPoints: [{ asDouble: purchaseMetrics.revenue, timeUnixNano: Date.now() * 1000000, attributes: [{ key: 'source', value: { stringValue: source } }] }],
-      },
-    },
-    {
-      name: 'jwt_pizza_system_cpu_percent',
-      unit: '%',
-      gauge: {
-        dataPoints: [{ asDouble: systemMetrics.cpu, timeUnixNano: Date.now() * 1000000, attributes: [{ key: 'source', value: { stringValue: source } }] }],
-      },
-    },
-    {
-      name: 'jwt_pizza_system_memory_percent',
-      unit: '%',
-      gauge: {
-        dataPoints: [{ asDouble: systemMetrics.memory, timeUnixNano: Date.now() * 1000000, attributes: [{ key: 'source', value: { stringValue: source } }] }],
-      },
-    },
-  ];
-
-  const payload = {
-    resourceMetrics: [
-      {
-        scopeMetrics: [
-          {
-            metrics,
-          },
-        ],
-      },
-    ],
-  };
-
-  try {
-    const response = await fetch(endpointUrl, {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${accountId}:${apiKey}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    });
-
-    if (!response.ok) {
-      const body = await response.text();
-      console.error(`Failed to push metrics data to Grafana: ${body}`);
-    }
-  } catch (err) {
-    console.error('Failed to send metrics:', err.message);
-  }
-}
-
 if (process.env.NODE_ENV !== 'test') {
   setInterval(() => {
     collectSystemMetrics();
     sendMetrics();
   }, 5000);
+}
 }
 
 module.exports = {
